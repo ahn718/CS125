78b5ac7b715368215d97b233faa55bf0ee851cb6
Sahil Modi <sm34524@gmail.com>
2017-09-29 10:45:30 -0500

    9/29/17 Lecture

--- Classwork/src/lecture/FunWithChars001.java
+++ Classwork/src/lecture/FunWithChars001.java
@@ -5,10 +5,17 @@ public class FunWithChars001 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for (int i = 0; i < 999; i++) {
-			System.out.printf("%5c", (char)i);
-			System.out.println();
+		for (int i = 0; i < 25; i++) {
+			for (int j = 0; j < 25; j++) {
+				System.out.println((char)(i + 'a') + "" + (char)(j + 'a'));
+				;
+			}
 		}
+
+		// for (int i = 0; i < 999; i++) {
+		// System.out.printf("%5c", (char)i);
+		// System.out.println();
+		// }
 	}
 
 }
---
+++ Classwork/src/lecture/OddArray.java
@@ -0,0 +1,27 @@
+package lecture;
+
+public class OddArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] oddArray1 = new int[10];
+		int[] oddArray2;
+
+		for (int i = 0; i < oddArray1.length; i++) {
+			oddArray1[i] = i * 2 + 1;
+		} // fills array with first 10 odd numbers;
+
+		oddArray2 = oddArray1;
+		oddArray1[4] = -999;
+		
+		for (int oddNum : oddArray1) { // only works for reading ENTIRE array
+			System.out.print(oddNum + " ");
+		}
+		System.out.println();
+		for (int oddNum : oddArray2) { // only works for reading ENTIRE array
+			System.out.print(oddNum + " ");
+		}
+	}
+
+}
