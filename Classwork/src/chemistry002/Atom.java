a9e7709ece36b5dac69fa36db17a4fe81ddf992f
Sahil Modi <sm34524@gmail.com>
2017-10-20 10:50:54 -0500

    Friday 10/20

---
+++ Classwork/src/chemistry002/Atom.java
@@ -0,0 +1,22 @@
+package chemistry002;
+
+public class Atom {
+
+	public double x, y, vx, vy;
+
+	public Atom(double x, double y, double vx, double vy) {
+		this.x = x;
+		this.y = y;
+		this.vx = vx;
+		this.vy = vy;
+	}
+
+	public boolean equals(Atom a2) {
+		return this.x == a2.x && this.y == a2.y;
+	}
+
+	public boolean moving() {
+		return this.vx != 0 || this.vy != 0;
+	}
+
+}
---
+++ Classwork/src/chemistry002/UniverseDriver.java
@@ -0,0 +1,22 @@
+package chemistry002;
+
+public class UniverseDriver {
+
+	public static void main(String[] args) {
+		Atom atom1 = new Atom(7.0, 10.0, 5.4, -13.2);
+		Atom atom2 = new Atom(8.0, 11.0, 6.2, -15);
+		Simulation sim = new Simulation();
+		
+		atom1.equals(atom2); //or smashing(atom1, atom2);
+		atom1.moving();
+		sim.init(100);
+	}
+	
+	public static boolean smashing(Atom a1, Atom a2) {
+		return a1.x == a2.x && a1.y == a2.y;
+	}
+	
+	
+	
+	
+}
